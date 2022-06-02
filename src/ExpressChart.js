39afffb0203f9aa0e80d71b34e8413b8ba993d82
Yin <yinyinkyaw13@gmail.com>
2022-06-02 23:14:16 +0630

    Modified ExprenseChartComponent

--- src/ExpressChart.js
+++ src/ExpressChart.js
@@ -1,45 +1,40 @@
+import { color } from 'highcharts';
 import React, {useEffect, useState} from 'react';
+import { BarChart, ResponsiveContainer, Bar, XAxis, Tooltip, Cell } from 'recharts';
 import "./App.css"
 import ExpandData from "./data.json";
-import HighchartsReact from "highcharts-react-official";
-import Highcharts from "highcharts";
 
 const ExpressChart = () => {
 
     const [totalExpand, setTotalExpand] = useState(0);
-    const expandData = ExpandData.filter((data)=> data.amount);
-
-    console.log("expand data::",expandData);
-
-    const options = {
-        chart: {
-            type:"column"
-        },
-        title: {
-            text:"",
-        },
-        xAxis: {
-            text:"",
-        },
-        yAxis:{
-            text: "",
-        },
-        series:[
-            {
-                data: expandData,
-            }
-        ]
-    }
+    const [currentIndex, setCurrentIndex] = useState(null);
 
     useEffect(()=>{
-        const data = ExpandData;
-        let totalAmount = 0;
-        for(let i = 0; i < data.length; i++){
-            totalAmount += data[i].amount;
-        }
-        setTotalExpand(totalAmount);
+        let expandAmount = 0;
+        for(let i=0; i< ExpandData.length; i++){
+            expandAmount += ExpandData[i].amount;
+        };
+        setTotalExpand(expandAmount);
+        // eslint-disable-next-line
     },[]);
 
+    const CustomizeTooltip = ({payload, label, active}) =>{
+    
+        return(
+            payload.length >= 1 &&
+            <div className="expense-chart-customize-tooltip">
+                ${payload[0].value}
+            </div>
+        )
+    }
+
+    function hoverBarChart(data, index,payload){
+        console.log("index:::",index,payload,data);
+        setCurrentIndex(1);
+
+    }
+
+    
     return (
         <div className="express-chart-component-container">
             <div className="express-chart-my-balance-container">
@@ -54,7 +49,19 @@ const ExpressChart = () => {
             <div className="express-chart-weekly-spend-chart-container">
                 <label>Spending - Last 7 days</label>
                 <div className="express-chart-weekly-spend-chart">
-                    <HighchartsReact highcharts={Highcharts} options={options} />
+                    <ResponsiveContainer width="100%" height="100%">
+                        <BarChart data={ExpandData} >
+                            <Bar dataKey={"amount"} onMouseOver={()=>{hoverBarChart()}} onMouseLeave={()=>{setCurrentIndex(ExpandData.length)}} >
+                                {
+                                    ExpandData.map((_,index)=>
+                                        <Cell cursor="pointer" fill={index === currentIndex ? "#ff9b87" :"#EC755D"} key={`cell-${index}`}  />
+                                    )
+                                }
+                            </Bar>
+                            <XAxis dataKey="day" axisLine={false} tickLine={false}/>
+                            <Tooltip content={<CustomizeTooltip/>} cursor={{fill:'transparent'}} offset={10}/>
+                        </BarChart>
+                    </ResponsiveContainer>
                 </div>
                 <div className="express-chart-weekly-spend--total-info">
                     <div>
